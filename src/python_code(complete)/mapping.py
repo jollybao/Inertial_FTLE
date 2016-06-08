d34b6fe98cdc5d225038f5a6958104f60b1361d7
jolly <jollybao@gmail.com>
2016-06-08 16:29:43 -0400

    magnetic force is added, and white noise is improved

--- src/python_code(complete)/mapping.py
+++ src/python_code(complete)/mapping.py
@@ -12,7 +12,7 @@ A = 0.1
 epsilon = 0.25
 w = 0.2*p
 Delta = 0.00001
-dt = 1e-1
+dt = 1e-2
 delta = dt
 T = 15
 L = 400
@@ -21,7 +21,7 @@ H = 200
 # Stokes' constant
 St = 0.3
 # Reynold's number
-R = 1
+R = 0
 St_inverse = 1/St
 
 # earth rotational speed
@@ -36,7 +36,10 @@ f = 2*w_rot*num.sin(phi)
 # parameters for Gaussian noise
 u = 0
 sigma = 1
-B = 1e-3
+B = 1e-2
+
+# paramters for magnetic force
+M = 5e-2
 
 start_time = time.time()
 
@@ -70,32 +73,37 @@ def accel(x,y,v,t):
     au = 1.5*R*u_acc(x,y,t)
     
     #Coriolis term
-    ac = num.column_stack((u[:,1],-1*u[:,0]))
-    ac = f*ac
+    #ac = num.column_stack((u[:,1],-1*u[:,0]))
+    #ac = f*ac
+
+    #Magnetic term
+    #am = M*x
     
-    a = a + au + ac
+    a = a + au
+    #a[:,1] += am
     return a
 
 # state transition function used in RK4 routine
-def update(state,t,noise):
+def update(state,t):
     r = state[:,2:4]
+    noise = B*num.random.normal(u,sigma,(L,2))
     v = accel(state[:,0],state[:,1],r,t) + noise
     return num.hstack((r,v))
 
     
 def rk4(state,t):
     r = state[:,0:4]
-    k1 = dt*update(r,t,state[:,4:6])
-    k2 = dt*update(r+0.5*k1,t+0.5*dt,state[:,4:6])
-    k3 = dt*update(r+0.5*k2,t+0.5*dt,state[:,4:6])
-    k4 = dt*update(r+k3,t+dt,state[:,4:6])
+    k1 = dt*update(r,t)
+    k2 = dt*update(r+0.5*k1,t+0.5*dt)
+    k3 = dt*update(r+0.5*k2,t+0.5*dt)
+    k4 = dt*update(r+k3,t+dt)
     r += (k1+2*k2+2*k3+k4)/6
     state[:,0] = num.clip(r[:,0],0.00001,1.99999)
     state[:,1] = num.clip(r[:,1],0.00001,0.99999)
 
     #noise = B*num.random.normal(u,sigma,(L,2))
     #state[:,4:6] += noise
-    return state[:,0:4]
+    return state
 
 '''
 r = num.array([[0.9,0.1],[1,0.4]],num.float64)
@@ -115,7 +123,7 @@ plt.plot(pts[:,0],pts[:,1])
 plt.show()
 '''
 
-output = open('w1_L.txt','ab')
+output = open('step001.txt','ab')
 # y-coordinate of the grid
 h = num.linspace(0.01,0.99,H,num.float64)
 #pts = num.zeros((H*L,2))
@@ -123,19 +131,19 @@ h = num.linspace(0.01,0.99,H,num.float64)
 for i in h:
     
     # initialize grid horizontally
-    state = num.zeros((L,6))
+    state = num.zeros((L,4))
     state[:,0] = num.linspace(0.01,1.99,L,num.float64)
     state[:,1] = i*num.ones(L,num.float64)
     state[:,2:4] = velocity(state[:,0],state[:,1],0)
     #state[:,4:6] = B*num.random.normal(u,sigma,(L,2))
-    state[:,4:6] = num.zeros((L,2))
+    #state[:,4:6] = num.zeros((L,2))
     
     #print(state)
     
     # perform RK4 to get position of particle 15s later
     for t in num.arange(0,T,dt):
   
-        state[:,0:4] = rk4(state,t)
+        state = rk4(state,t)
         
     #pts[j:L+j,:] = state[:,0:2]
     #j = j + L
